401c01a7bf251d810c8f17167b746b7d385ad6b0
Eric Schulte <schulte.eric@gmail.com>
2013-12-05 15:40:31 -0700

    defun and setf support

--- pseudo-print.lisp
+++ pseudo-print.lisp
@@ -29,13 +29,27 @@
   (declare (ignorable colon? atsign?))
   (format s "~:@_~0@TIf ~a Then~:@_" (second r))
   (format s "~2@T~W~:@_"(third r))
-  (format s "~0@TElse~:@_")
-  (format s "~2@T~W~:@_"(fourth r))
-  (format s "~0@TEndIf~:@_"))
+  (when (fourth r)
+    (format s "~0@TElse~:@_")
+    (format s "~2@T~W~:@_"(fourth r)))
+  (format s "~0@TEndIf"))
 
 (defun infix-print (s r colon? atsign?)
   (declare (ignorable colon? atsign?))
   (format s (format nil "(~~{~~W~~^ ~W ~~})" (first r)) (cdr r)))
+
+(defun defun-print (s r colon? atsign?)
+  (declare (ignorable colon? atsign?))
+  (format s "~:@_~0@TFunction: ~a(~{~a~^, ~})" (second r) (third r))
+  (format s "~2I~{~W~^~:@_~}" (cdddr r)))
+
+(defun setf-print (s r colon? atsign?)
+  (declare (ignorable colon? atsign?))
+  (format s "~{~{~W <- ~W~}~^~:@_~}"
+          (loop
+             :for i :from 0 :by 2 :below (length (cdr r))
+             :for j :from 1 :by 2 :below (length (cdr r))
+             :collect (list (nth i (cdr r)) (nth j (cdr r))))))
 
 (defmacro with-pseudo-pprinter (&rest body)
   (let ((orig-tab (gensym)))
@@ -47,6 +61,10 @@
                                    (formatter "~/pseudo-print::if-print/"))
               (set-pprint-dispatch '(cons (and symbol (member > < = + - * /)))
                                    (formatter "~/pseudo-print::infix-print/"))
+              (set-pprint-dispatch '(cons (and symbol (eql defun)))
+                                   (formatter "~/pseudo-print::defun-print/"))
+              (set-pprint-dispatch '(cons (and symbol (eql setf)))
+                                   (formatter "~/pseudo-print::setf-print/"))
               ,@body)
          ;; restore original value of pprinter
          (setq *print-pprint-dispatch* ,orig-tab)))))
