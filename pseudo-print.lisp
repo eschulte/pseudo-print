e3dfdca290aed6fab7dae7f73e96ec311bd19809
Eric Schulte <schulte.eric@gmail.com>
2013-12-05 12:08:12 -0700

    using existing pretty print machinery

--- pseudo-print.lisp
+++ pseudo-print.lisp
@@ -7,9 +7,41 @@
 ;;; Commentary
 
 ;; The `pseudo-print' function prints lisp code as pseudo code.
-;; Customize by changing the value of the `pseudo-printing-table.'
 
 ;;; Code:
 (in-package :pseudo-print)
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (enable-curry-compose-reader-macros))
+
+(defvar pseudo-pprinters nil
+  "List of pseudo-printer functions.")
+
+(defun if-print (s r colon? atsign?)
+  (declare (ignorable colon? atsign?))
+  (pprint-tab :line 1 2 s)
+  (format s "If ~a Then" (second r))
+  (pprint-newline :mandatory s)
+  (pprint-tab :line 3 2 s)
+  (pprint-linear s (third r))
+  (pprint-newline :mandatory s)
+  (pprint-tab :line 1 2 s)
+  (format s "Else")
+  (pprint-newline :mandatory s)
+  (pprint-tab :line 3 2 s)
+  (pprint-linear s (third r))
+  (pprint-newline :mandatory s)
+  (pprint-tab :line 1 2 s)
+  (format s "EndIf")
+  (pprint-newline :mandatory s))
+
+(set-pprint-dispatch
+ '(cons (and symbol (eql if)))
+ (formatter "~/pseudo-print::if-print/"))
+
+(defmacro with-pseudo-pprinter (&rest body)
+  `(unwind-protect
+        (progn
+          
+          ,@body)
+     ;; restore original value of pprinter
+     (setq *print-pprint-dispatch* (copy-pprint-dispatch nil))))
