2bd209fe2dda02be021da0dad8c275ae8af10b02
Eric Schulte <schulte.eric@gmail.com>
2013-12-05 11:41:36 -0700

    thinking of doing with pattern matching

--- package.lisp
+++ package.lisp
@@ -1,3 +1,3 @@
 (defpackage #:pseudo-print
-  (:use :common-lisp :alexandria :curry-compose-reader-macros)
+  (:use :common-lisp :alexandria :curry-compose-reader-macros :cl-match)
   (:export :pseudo-print))
